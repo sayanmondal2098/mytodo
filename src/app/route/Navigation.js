109c157cc4a558e6f930a59c743fcd1dfa4e4e8f
Sayan Mondal <sayanmondal2098@gmail.com>
2020-02-25 23:18:12 +0530

    sidebar css added

--- src/app/route/Navigation.js
+++ src/app/route/Navigation.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import '../../assect/css/mycss.css';
+
 class Navigation extends React.Component{
     render(){
         return(
-            <dev>
+            <dev className="sidenav">
                 <NavLink to="/home">Home</NavLink>
                 <NavLink to="/user">User</NavLink>
                 <NavLink to="/root">Root</NavLink>
